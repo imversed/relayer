451e5049d51e24d05102e131d56a742c6c4b349c
Marina Gonokhina <marina.gonokhina@fulldive.com>
2022-04-22 10:59:37 +0300

    import key test

--- chain_client/imversed/keys_test.go
+++ chain_client/imversed/keys_test.go
@@ -1,29 +1,53 @@
 package client_test
 
 import (
+	imvclient "github.com/cosmos/relayer/v2/chain_client/imversed"
+	imvprovider "github.com/cosmos/relayer/v2/relayer/provider/imversed"
+
+	"github.com/cosmos/relayer/v2/chain_client/imversed/crypto/hd"
+	"os"
 	"testing"
 
-	"github.com/strangelove-ventures/lens/client"
 	"go.uber.org/zap/zaptest"
 )
 
 // TestKeyRestore restores a test mnemonic
 func TestKeyRestore(t *testing.T) {
 	keyName := "test_key"
-	mnemonic := "blind master acoustic speak victory lend kiss grab glad help demand hood roast zone lend sponsor level cheap truck kingdom apology token hover reunion"
-	expectedAddress := "cosmos15cw268ckjj2hgq8q3jf68slwjjcjlvxy57je2u"
+	mnemonic := "empower bounce moon grain plug brisk anchor breeze van submit task develop blush lottery border dish card moment drum pull glass must vicious main"
+	expectedAddress := "imv1x70p5l9ntluxu6kmkj6wg44mlkzku70e8fx222"
 	var coinType uint32
 	coinType = 118 // Cosmos coin type used in address derivation
 
 	homepath := t.TempDir()
-	cl, err := client.NewChainClient(
+
+	config := imvprovider.ImversedProviderConfig{
+		Key:            "imversed-local-key",
+		ChainID:        "imversed_1234-1",
+		RPCAddr:        "http://localhost:26657",
+		AccountPrefix:  "imv",
+		KeyringBackend: "test",
+		GasAdjustment:  1.3,
+		GasPrices:      "0.001nimv",
+		Debug:          true,
+		Timeout:        "10s",
+		OutputFormat:   "json",
+		SignModeStr:    "direct",
+	}
+
+	cl, err := imvclient.NewChainClient(
 		zaptest.NewLogger(t),
-		client.GetCosmosHubConfig(homepath, true),
-		homepath, nil, nil,
+		imvprovider.ImvChainClientConfig(&config),
+		homepath,
+		os.Stdin,
+		os.Stdout,
+		hd.EthSecp256k1Option(),
 	)
+
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	_ = cl.DeleteKey(keyName) // Delete if test is being run again
 	address, err := cl.RestoreKey(keyName, mnemonic, coinType)
 	if err != nil {
